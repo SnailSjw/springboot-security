ceca62f255e1a1c66ac22f5727489b1498dd18e5
snailjw <1026405351@qq.com>
2020-07-02 22:07:52 +0800

    springboot整合Security学习

--- src/main/java/com/snialjw/config/SecurityConfig.java
+++ src/main/java/com/snialjw/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.snialjw.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//@Configuration
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,5 +16,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level1/**").hasRole("vip1")
                 .antMatchers("/level2/**").hasRole("vip2")
                 .antMatchers("/level3/**").hasRole("vip3");
+        //没有权限默认跳转到登录页面，需要开启登录的页面
+        http.formLogin();
+
+        //注销
+        http.logout().logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("snailjw").password(new BCryptPasswordEncoder().encode("123123")).roles("vip1")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123123")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("123123")).roles();
     }
 }
